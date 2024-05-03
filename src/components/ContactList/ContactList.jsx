fbf80c2b5b49911888feec7bc24da9a22a928de3
OlenaYermak <145419464+OlenaYermak@users.noreply.github.com>
2024-05-03 08:27:52 +0300

    re

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,15 @@
+import Contact from "../Contact/Contact"
+
+import css from "./ContactList.module.css"
+
+export default function ContactList({ contacts, onDelete }) {
+    return (
+        <ul className={css.contactList}>
+            {contacts.map((contact) => (
+                <li className={css.contactContainer} key={contact.id}>
+                    <Contact nameContact={contact.name} numberContact={contact.number} onDelete={onDelete} id={ contact.id} />
+                </li>
+            ))}
+        </ul>
+    );
+}
